735e0320fb8881e2d7bf4120f50ed9f94dbaa0a4
Martin Unger <martin-unger2@live.at>
2023-10-30 10:08:00 +0100

    social meida btn atf

--- src/app/atf-section/atf-section.component.ts
+++ src/app/atf-section/atf-section.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./atf-section.component.scss']
 })
 export class AtfSectionComponent {
-
+  socialMedia = ['github.svg', 'mail.svg', 'linkedin.svg']
 }
