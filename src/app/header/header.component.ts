643bf61cfa9791150cbabbbb4a54864e6517cbb2
Martin Unger <126020447+MartinUnger98@users.noreply.github.com>
2023-11-08 15:50:49 +0000

    burger menu start responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @ViewChild('burgerimg') burgerimg!: ElementRef;
+  @ViewChild('menu') menu!: ElementRef;
+
+  burgerMenuActive = false;
+
+  toggleBurgerMenu() {
+    this.toggleImg();
+    this.toggleMenu();
+  }
+
+
+  toggleImg() {
+    let burgerImg = this.burgerimg.nativeElement;
+    if (this.burgerMenuActive === false) {
+      burgerImg.src = 'assets/icons/burgermenuclose.svg';
+      this.burgerMenuActive = true;
+    } else {
+      burgerImg.src = 'assets/icons/burgermenu.svg';
+      this.burgerMenuActive = false;
+    }
+    
+  }
+
+  toggleMenu() {
+    let menu = this.menu.nativeElement;
+    if (menu.classList.contains('menu-visible')) {
+      this.menuFadeOut(menu);
+    } else {
+      this.menuFadeIn(menu);
+    }
+  } 
+
+  menuFadeIn(menu: HTMLElement) {
+    menu.classList.add('menu-visible');
+    menu.classList.remove('menu-hidden');
+  }
+
+  menuFadeOut(menu: HTMLElement) {
+    menu.classList.add('menu-hidden');
+    menu.classList.remove('menu-visible');
+  }
 
 }
