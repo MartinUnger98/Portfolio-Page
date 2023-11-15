56dc2fafcb94c80a34855a457eda2bbc1d514867
Martin Unger <126020447+MartinUnger98@users.noreply.github.com>
2023-11-15 09:36:58 +0000

    routing + privacy policy

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page',
-  templateUrl: './page.component.html',
-  styleUrls: ['./page.component.scss']
+  styleUrls: ['./page.component.scss'],
+  template: `
+  <app-atf-section></app-atf-section>
+  <app-about-me></app-about-me>
+  <app-skills></app-skills>
+  <app-projects></app-projects>
+  <app-contact></app-contact>
+  `,
 })
 export class PageComponent {
 
