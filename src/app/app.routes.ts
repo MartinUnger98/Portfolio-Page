195f555579a2a2b20feb7fb23f754e56af5487bc
Martin Unger <martin-unger2@live.at>
2025-08-25 22:32:47 +0200

    added tailwind + basic routing + fonts + first components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  //{ path: 'legal-notice', component: LegalNoticeComponent },
+  //{ path: 'privacy-policy', component: PrivacyPolicyComponent },
+
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/components/hero-section/hero-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [],
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.scss'
+})
+export class HeroSectionComponent {
+
+}
